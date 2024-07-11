2c43327e61a7a96e4ca3ec811b10fb7c4dc83d87
matiasemmanuelpuyo1989 <matias.puyo7@gmail.com>
2024-07-11 00:34:53 -0300

    modifique el header

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const path = require("node:path");
 const app= express();
 
-const port = 10000 || process.env.PORT;
+const port = 3000 || process.env.PORT;
 
 app.get("/",(req, res)=>{
     res.sendFile(path.resolve("views/home.html"));
@@ -15,6 +15,6 @@ app.use(express.static("./public"));
 app.get('/register',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/register.html'));
 });
-app.get('/login',(req,res)=>{
+app.get("/login",(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/login.html'));
 });
